970565dcf492ee715257be596acbb9f3aac6db8f
mrfurm4n <mr.furmand@gmail.com>
2022-10-29 20:02:39 +0200

    rebase tests to Jest

---
+++ __tests__/capitalize.test.js
@@ -0,0 +1,9 @@
+import { capitalize } from "../src/capitalize.js";
+
+test(
+    'capitalize',
+    () => {
+        expect(capitalize('hello')).toEqual('Hello');
+        expect(capitalize('')).toEqual('');
+    }
+)
--- tests/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import { strict as assert } from 'node:assert';
-import { capitalize } from "../src/capitalize.js";
-
-assert.equal(capitalize('hello'), 'Hello');
-assert.equal(capitalize(''), '');
-
-console.log('Все тесты пройдены!');
